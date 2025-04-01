d1e49d64ab6f2369c1634dca335c57e692140b8b
KAABSHAHID <118444239+KAABSHAHID@users.noreply.github.com>
2025-04-01 17:33:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -175,8 +175,8 @@ def img_text():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    #app.run(host='0.0.0.0', port=5000)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
 
 
 
